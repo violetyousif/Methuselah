612ab960f5227ff0737377b2af9f97a4475897de
HorusBorus <viktorgjorgjevski359@gmail.com>
2025-06-03 14:19:28 -0400

    Viktor Gjorgjevski 06/03/2025 -added profile picture support and fixed token based logout issues

---
+++ src/components/profilePicker.tsx
@@ -0,0 +1,8 @@
+//Edited by Viktor Gjorgjevski
+//06/03/2025
+//array of present avatars for a user to choose from
+export const profilePicPresets = [
+  '/avatars/avatar1.png',
+  '/avatars/avatar2.png',
+  '/avatars/avatar3.png',
+];
