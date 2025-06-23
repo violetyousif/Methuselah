03fdc171ae51157bb76b26816dce05bbda58deef
Violet Yousif <95271451+violetyousif@users.noreply.github.com>
2025-06-23 18:59:33 -0400

    Update login.tsx
    Removed database dummy username + password so it's not posted on a public repo

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -17,12 +17,6 @@ import Link from 'next/link';
 import { ArrowLeftOutlined, VerticalAlignMiddleOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/router';
 
-//test account
-//username: mothergothel@example.com
-//password: MotherIsDisney11!
-
-
-
 function Login() {
   const router = useRouter();
   const [form] = Form.useForm();
