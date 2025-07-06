70877e0527772233fbafa283fc514bb8258cf9a3
Syed Rabbey <hh6071@wayne.edu>
2025-07-06 19:43:57 -0400

    Fix fix fix.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -34,7 +34,7 @@ function Login() {
 
   const onFinish = async (values: any) => {
     try {
-      const response = await fetch('http://localhost:8080/api/auth/login', {
+      const response = await fetch('http://localhost:8080/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',     // Include cookies for session management request
