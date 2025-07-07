2bc6d510c2dc6a378049343e103c4a426ae69e2e
Mohammad Hoque <mohoquee@gmail.com>
2025-07-06 22:22:35 -0400

    Import express-rate-limit to enhance rate limiting functionality for user login routes

--- backend/routes/userLogin.js
+++ backend/routes/userLogin.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.join(__dirname, '../.env.local') });
 
-
+import rateLimit from 'express-rate-limit';
 import express from 'express';
 const router = express.Router();
 import User from '../models/User.js'; 
