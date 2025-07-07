7155e6668e322ceab834a7f3288202d6c611d9fc
Mohammad Hoque <mohoquee@gmail.com>
2025-07-07 11:25:09 -0400

    Add express-session and update package dependencies

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ import cors from 'cors';          // for cross origin requests
 import bcrypt from 'bcrypt';      // to encrypt passwords
 import jwt from 'jsonwebtoken';   // security: make sure user is logged in to access app session
 import { fileURLToPath } from 'url';
+import session from 'express-session';
  
 // Importing routes and middleware
 import logger from './middleware/logger.js';
@@ -64,7 +65,7 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' })); // Increase from default 100kb to 10mb
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json());
-app.use(lusca.csrf()); // Add CSRF protection middleware
+//app.use(lusca.csrf()); // Add CSRF protection middleware
 //app.use('/api/auth', userLoginRoutes);
 app.use(logger);  // Logs all incoming requests
 
